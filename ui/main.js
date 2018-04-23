d74d3fdeffd3af52466c2a1689331ea22a599d8b
Priyadharshni R <rpriya185@gmail.com>
2018-04-23 20:55:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,7 @@ button.onclick=function() {
       // if(request.status === 200){
           // var counter = request.responseText;
           counter=counter+1;
-          var span = document.getElementById('count');
+          var span = document.getElementById("count");
           span.innerHTML = counter.toString();
           
       // }
