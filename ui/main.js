e50acfc3aa0ff4d80b6de51dd67830fc67cbd002
Priyadharshni R <rpriya185@gmail.com>
2018-04-23 20:57:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,19 +2,19 @@
 var button = document.getElementById("counter");
 var counter=0;
 button.onclick=function() {
-  //var request = new XMLHttpRequest();
-  //request.onreadystatechange = function() {
-   //if(request.readyState === XMLHttpRequest.DONE)  {
-      // if(request.status === 200){
-          // var counter = request.responseText;
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = function() {
+   if(request.readyState === XMLHttpRequest.DONE)  {
+       if(request.status === 200){
+           var counter = request.responseText;
           counter=counter+1;
           var span = document.getElementById("count");
           span.innerHTML = counter.toString();
           
-      // }
-  // }
-// };
- // request.open('GET','http://rpriya185.imad.hasura-app.io/counter',true);
- // request.send(null);
+       }
+   }
+};
+  request.open('GET','http://rpriya185.imad.hasura-app.io/counter',true);
+  request.send(null);
     
 };
