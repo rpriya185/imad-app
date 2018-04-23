d56c8eddc319e53ccb04abb681da78bac24b8ab0
Priyadharshni R <rpriya185@gmail.com>
2018-04-23 20:36:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 var button = document.getElementById('counter');
 var counter=0;
-button.onClick=function() {
+button.onclick=function() {
   var request = new XMLHttpRequest();
   request.onreadystatechange = function()
  {
